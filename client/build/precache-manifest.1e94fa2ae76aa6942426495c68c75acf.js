d2978bf14015f57b93282856fad5bb16fbf08456
Verde <a.green.designer@gmail.com>
2019-04-18 20:00:18 -0400

    deploying

---
+++ client/build/precache-manifest.1e94fa2ae76aa6942426495c68c75acf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df90e95fa7c08283357b",
+    "url": "/static/js/main.0ac630e7.chunk.js"
+  },
+  {
+    "revision": "2df0352414ec0dde7a84",
+    "url": "/static/js/2.d4f7ed59.chunk.js"
+  },
+  {
+    "revision": "df90e95fa7c08283357b",
+    "url": "/static/css/main.ac532739.chunk.css"
+  },
+  {
+    "revision": "b4fedff28ed2b194394dc2fbdde7f0da",
+    "url": "/index.html"
+  }
+];
